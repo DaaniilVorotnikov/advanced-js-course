a6969f43dc809fded844b2707ea7562292699e7d
dvorotnikov <dvorotnikov@diasoft.ru>
2025-01-12 23:43:04 +0300

    Блок 6.1 настройка loki для скриншотных тестов

--- src/pages/main-page/ui/main-page.stories.tsx
+++ src/pages/main-page/ui/main-page.stories.tsx
@@ -8,7 +8,7 @@ export default {
     component: MainPage,
 } as ComponentMeta<typeof MainPage>
 
-const Template: ComponentStory<typeof MainPage> = (args) => <MainPage {...args} />;
+const Template: ComponentStory<typeof MainPage> = (args:any) => <MainPage {...args} />;
 
 export const Light = Template.bind({})
 
