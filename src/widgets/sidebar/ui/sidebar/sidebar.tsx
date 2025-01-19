6d237b0bdd4be8659ad401b02b932cd23201764e
dvorotnikov <dvorotnikov@diasoft.ru>
2025-01-19 18:10:13 +0300

    Блок 7 настройка пайплайна

--- src/widgets/sidebar/ui/sidebar/sidebar.tsx
+++ src/widgets/sidebar/ui/sidebar/sidebar.tsx
@@ -3,11 +3,13 @@ import { useState } from 'react';
 import { ThemeSwitcher } from 'shared/ui/theme-switcher';
 import { LangSwitcher } from 'shared/ui/lang-switcher/lang-switcher';
 import cls from './sidebar.module.scss';
+import { useTranslation } from 'react-i18next';
 
 interface SidebarProps {className?:string;}
 
 export const Sidebar = ({ className }: SidebarProps) => {
     const [collapsed, setCollapsed] = useState(false);
+    const {t} = useTranslation()
     const onToggle = () => {
         setCollapsed((prev) => !prev);
     };
@@ -18,7 +20,7 @@ export const Sidebar = ({ className }: SidebarProps) => {
             className={classNames(cls.sidebar, { [cls.collapsed]: collapsed }, [className])}
         >
             <button data-testid="sidebar-toggle" onClick={onToggle}>
-                toggle
+                {t('Переключить')}
             </button>
             <div className={cls.switchers}>
                 <ThemeSwitcher />
