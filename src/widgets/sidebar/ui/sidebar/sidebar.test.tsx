8dde0a6798dd235e0cb403ef18422a09c741be02
dvorotnikov <dvorotnikov@diasoft.ru>
2025-03-06 20:00:19 +0300

    Блок 8 дебаг тестов

--- src/widgets/sidebar/ui/sidebar/sidebar.test.tsx
+++ src/widgets/sidebar/ui/sidebar/sidebar.test.tsx
@@ -3,15 +3,16 @@ import React from 'react';
 import { withTranslation } from 'react-i18next';
 import { Sidebar } from 'widgets/sidebar/ui/sidebar/sidebar';
 import { renderWithTranslation } from 'shared/lib/tests/renderWithTranslation/renderWithTranslation';
+import { componentRender } from 'shared/config/tests/component-render/component-render';
 
 describe('Sidebar', () => {
     test('with only first param', () => {
-        renderWithTranslation(<Sidebar />);
+        componentRender(<Sidebar />);
         expect(screen.getByTestId('sidebar')).toBeInTheDocument();
     });
 
     test('test toggle', () => {
-        renderWithTranslation(<Sidebar />);
+        componentRender(<Sidebar />);
         const toggleBtn = screen.getByTestId('sidebar-toggle');
         expect(screen.getByTestId('sidebar')).toBeInTheDocument();
         fireEvent.click(toggleBtn)
